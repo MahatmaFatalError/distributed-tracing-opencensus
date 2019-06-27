6f8851ef32e50972f174bb27e483b57a4fa80c84
D072521 <julian.ruppel@sap.com>
2019-06-28 00:05:44 +0200

    wip

--- postgres-log-to-opencensus-converter/src/main/java/cloud/jindujun/RowHandler.java
+++ postgres-log-to-opencensus-converter/src/main/java/cloud/jindujun/RowHandler.java
@@ -1,5 +1,6 @@
 package cloud.jindujun;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -22,6 +23,7 @@ import io.opencensus.trace.SpanContext;
 import io.opencensus.trace.Tracer;
 import io.opencensus.trace.Tracing;
 import io.opencensus.trace.propagation.SpanContextParseException;
+import io.opencensus.trace.samplers.Samplers;
 
 public class RowHandler {
 
@@ -59,12 +61,12 @@ public class RowHandler {
 	/*
 	 * Map<pid,traceContext>
 	 */
-	Map<String, String> pidTraceMapping = new HashMap<>();
+	Map<Integer, String> pidTraceMapping = new HashMap<>();
 
 	/*
 	 * Map<pid, Map<TableId,TimestampTuple>>
 	 */
-	Map<String, Map<String, TimestampTuple>> gatheredTimeSpans = new HashMap<>();
+	Map<Integer, Map<String, TimestampTuple>> gatheredTimeSpans = new HashMap<>();
 
 	private static final Tracer tracer = Tracing.getTracer();
 
@@ -72,54 +74,61 @@ public class RowHandler {
 
 	private TraceContextLoader traceContextLoader = new TraceContextLoader();
 
-	private Map<String, String> waitingPids = new HashMap<>();
+	private Map<Integer, ZonedDateTime> waitingPids = new HashMap<>();
 
 	public void handleRow(Map<String, Object> row) {
 		String message = (String) row.get("message");
-		String timestamp = (String) row.get("log_time");
-		String pid = (String) row.get("process_id");
+		java.sql.Timestamp sqlTimestamp = (java.sql.Timestamp) row.get("log_time");
+		Integer pid = (Integer) row.get("process_id");
 		LOG.info(message);
+
+		ZonedDateTime timestamp = ZonedDateTime.ofInstant(sqlTimestamp.toInstant(), ZoneId.systemDefault());
 		convert(message, timestamp, pid);
 	}
 
-	public void convert(String message, String timestamp, String pid) {
+	public void convert(String message, ZonedDateTime timestamp, Integer pid) {
 		SpanBuilder spanBuilder = null;
+		SpanContext spanContext = null;
 
 		if (message != null && message.contains("-- SpanContext{traceId=TraceId{traceId=")) {
 
-			String spanName = "Holger";
-
 			try {
-				SpanContext spanContext = traceContextLoader.loadSpanContext(message);
-
-				spanBuilder = tracer.spanBuilderWithRemoteParent(spanName, spanContext).setRecordEvents(true);
-
-				pidTraceMapping.put(pid, spanContext.getTraceId().toString());
+				spanContext = traceContextLoader.loadSpanContext(message);
 			} catch (SpanContextParseException e) {
-				spanBuilder = tracer.spanBuilder(spanName).setRecordEvents(true);
+				// spanBuilder =
+				// tracer.spanBuilder(spanName).setRecordEvents(true).setSampler(Samplers.alwaysSample());
 				LOG.warn("Parent Span is not present");
 			}
 
-			Span tempspan = spanBuilder.startSpan();
-			RecordEventsSpanImpl span = (RecordEventsSpanImpl) tempspan;
-
-			// https://github.com/census-instrumentation/opencensus-java/issues/1905
-			// https://github.com/census-instrumentation/opencensus-java/pull/1906
-
 			Map<String, TimestampTuple> relationTsTupleMap = gatheredTimeSpans.get(pid);
-			TimestampTuple timestampTuple = relationTsTupleMap.get("TODO_RELATION");
+			if (relationTsTupleMap != null) {
 
-			Timestamp startTsConverted = Timestamp.create(timestampTuple.getStartTs().toInstant().getEpochSecond(),
-					timestampTuple.getStartTs().toInstant().getNano());
-			Timestamp endTsConverted = Timestamp.create(timestampTuple.getEndTs().toInstant().getEpochSecond(),
-					timestampTuple.getEndTs().toInstant().getNano());
+				spanBuilder = tracer.spanBuilderWithRemoteParent("wait for lock", spanContext).setRecordEvents(true)
+						.setSampler(Samplers.alwaysSample());
 
-			span.setStartTime(startTsConverted);
+				pidTraceMapping.put(pid, spanContext.getTraceId().toString());
 
-			try (Scope ws = tracer.withSpan(span)) {
-				span.addAnnotation(message);
-			} finally {
-				span.end(EndSpanOptions.DEFAULT, endTsConverted);
+				Span tempspan = spanBuilder.startSpan();
+				RecordEventsSpanImpl span = (RecordEventsSpanImpl) tempspan;
+
+				// https://github.com/census-instrumentation/opencensus-java/issues/1905
+				// https://github.com/census-instrumentation/opencensus-java/pull/1906
+
+				TimestampTuple timestampTuple = relationTsTupleMap.get("TODO_RELATION");
+
+				Timestamp startTsConverted = Timestamp.create(timestampTuple.getStartTs().toInstant().getEpochSecond(),
+						timestampTuple.getStartTs().toInstant().getNano());
+				Timestamp endTsConverted = Timestamp.create(timestampTuple.getEndTs().toInstant().getEpochSecond(),
+						timestampTuple.getEndTs().toInstant().getNano());
+
+				span.setStartTime(startTsConverted);
+
+				try (Scope ws = tracer.withSpan(span)) {
+					span.addAnnotation(message);
+					gatheredTimeSpans.remove(pid);
+				} finally {
+					span.end(EndSpanOptions.DEFAULT, endTsConverted);
+				}
 			}
 
 		}
@@ -134,8 +143,7 @@ public class RowHandler {
 		if (message != null && message.contains("still waiting for")) {
 
 			Map<String, TimestampTuple> value = new HashMap<>();
-			ZonedDateTime startTs = ZonedDateTime.parse(timestamp, formatter);
-			TimestampTuple tsTuple = new TimestampTuple().setStartTs(startTs);
+			TimestampTuple tsTuple = new TimestampTuple().setStartTs(timestamp);
 			value.put("TODO_RELATION", tsTuple);
 
 			gatheredTimeSpans.put(pid, value);
@@ -144,12 +152,10 @@ public class RowHandler {
 
 		} else if (message != null && message.contains(" acquired ") && waitingPids.containsKey(pid)) {
 
-			ZonedDateTime endTs = ZonedDateTime.parse(timestamp, formatter);
-
 			Map<String, TimestampTuple> relationTsTupleMap = gatheredTimeSpans.get(pid);
 			TimestampTuple timestampTuple = relationTsTupleMap.get("TODO_RELATION");
-			timestampTuple.setEndTs(endTs);
-			relationTsTupleMap.put(pid, timestampTuple);
+			timestampTuple.setEndTs(timestamp);
+			relationTsTupleMap.put("TODO_RELATION", timestampTuple);
 
 		}
 	}
---
+++ postgres-log-to-opencensus-converter/src/main/java/cloud/jindujun/config/JaegerConfig.java
@@ -0,0 +1,14 @@
+package cloud.jindujun.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import io.opencensus.exporter.trace.jaeger.JaegerTraceExporter;
+
+@Configuration
+public class JaegerConfig {
+
+	public JaegerConfig(@Value("${tracing.jaegerUrl}") String jaegerThriftEndpoint) {
+		JaegerTraceExporter.createAndRegister(jaegerThriftEndpoint, "log-converter");
+	}
+}
